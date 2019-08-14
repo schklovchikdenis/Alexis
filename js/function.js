f1948c0f6e8b7fa52419a41b10d6287b491d0102
den <sc@mail.ru>
2019-08-14 12:13:10 +0500

    New file

--- js/function.js
+++ js/function.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {  
+$(document).ready(function() {
       $('.slider__slid').slick({
       infinite: true,
       slidesToShow: 2,
       slidesToScroll: 1,
         nextArrow: '<img src="img/arowSlid.png" alt="" />',
-          prevArrow: '<div class="prev"></div>'          
+          prevArrow: '<div class="prev"></div>'
       });
 });
